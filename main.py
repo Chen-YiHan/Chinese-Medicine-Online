7c32e675835c4adb70c1e0931d3039124f20562e
xingyunjohn1 <xingyunjohn1@qq.com>
2023-03-08 23:11:56 +0800

    update search in main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ st.header("欢迎使用中药材知识学习和考试系统")
 "*本系统由陈一翰[@Chen-YiHan](https://github.com/Chen-YiHan)制作，方利国老师指导*"
 "[进入知识学习](https://chen-yihan-chinese-medicine-online-zhishixuexi-a9dqao.streamlit.app/)"
 "[进入考试系统](https://chen-yihan-chinese-medicine-online-examination-evy99w.streamlit.app/)"
+"[进入模糊搜索](https://chen-yihan-chinese-medicine-online-search-ffojfj.streamlit.app/)"
